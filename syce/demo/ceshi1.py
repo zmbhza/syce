92d306b61644b9a3bba8b0db626146db8ded42c9
zds <847160625@qq.com>
2020-09-27 10:38:46 +0800

    "第一次提交"

--- syce/demo/ceshi1.py
+++ syce/demo/ceshi1.py
@@ -16,11 +16,37 @@ def address():
     second_name = ["伟", "华", "建国", "洋", "刚", "万里", "爱民", "牧", "陆", "路", "昕", "鑫", "兵", "硕", "志宏", "峰", "磊", "雷", "文","明浩", "光", "超", "军", "达"]
     name = (random.choice(first_name) + random.choice(second_name)+random.choice(first_name)+random.choice(second_name)+random.choice(first_name) + random.choice(second_name)+random.choice(first_name) + random.choice(second_name))*2
     return name
-s = 0
-a = 1
-while  name():
+# s = 0
+# a = 1
+# while  name():
+#
+#     s+=a
+#     print (str(name()),s)
+#     if name()=='左建国马建国左达王鑫':
+#         break
 
-    s+=a
-    print (str(name()),s)
-    if name()=='左建国马建国左达王鑫':
-        break
+print('小精灵：您好 ，欢迎来到古灵阁，请问您需要帮助吗，需要or不需要？')
+a = input()
+if a =='需要':
+    b = input('请问您需要什么帮助？1.存款 2.货币兑换 3.咨询\n')
+    if b =='1':
+        print('小精灵：滚')
+    elif b == '2':
+        print('小精灵：金加隆和人民币的汇率为1:51.3，即一金加隆=51.3人民币')
+        print('小精灵：请问您需要兑换多少金加隆呢')
+        c = input()
+        try:
+            try:
+                s = int(c)
+                print('小精灵：好的，我知道了，您需要兑换'+str(s)+'金加隆')
+                print('小精灵：那么您需要付给我'+str(s*51.3)+'人民币')
+            except:
+                s = float(c)
+                print('小精灵：好的，我知道了，您需要兑换' + str(s) + '金加隆')
+                print('小精灵：那么您需要付给我' + str(s * 51.3) + '人民币')
+        except:
+            print('滚')
+    elif b == '3':
+        print('小精灵：滚')
+else:
+    print('小精灵：滚')
--- syce/demo/ceshi3.py
+++ syce/demo/ceshi3.py
@@ -1,37 +1,93 @@
-import xlrd
-from xlutils.copy import copy
-# #读取文件
-# read_file = xlrd.open_workbook('C:\\Users\\zhao\\Desktop\\xiugai.xlsx',formatting_info=True)
-# #参数注释：
-# #file_path：文件路径，包含文件的全名称
-# #formatting_info=True：保留Excel的原格式
-#
-# #将文件复制到内存
-# write_data = copy(read_file)
-#
-# #读取复制后文件的sheet1
-# write_save = write_data.get_sheet(0)
-#
-# #写入数据
-# write_save.write(1,0,'v1alue')
-# #参数注释：
-# #x,y:写入目标格的位置坐标
-# #value：写入数据
-#
-# #保存写入数据后的文件到原文件路径
-# write_data.save('C:\\Users\\zhao\\Desktop\\xiugai.xlsx')
-def ede(a,b,c,d):
-    # 参数注释：
-    # file_path：文件路径，包含文件的全名称
-    # formatting_info=True：保留Excel的原格式
-    read_file = xlrd.open_workbook(a, formatting_info=True)
-    # 将文件复制到内存
-    write_data = copy(read_file)
-    # 读取复制后文件的sheet1
-    write_save = write_data.get_sheet(0)
-    # 写入数据
-    write_save.write(b, c, d)
-    # 保存写入数据后的文件到原文件路径
-    write_data.save(a)
+# 用Pyaudio库录制音频
+#   out_file:输出音频文件名
+#   rec_time:音频录制时间(秒)
+import pyaudio
+import wave
+from aip import AipSpeech
+import time
+""" 你的 APPID AK SK """
+APP_ID = '22752470'
+API_KEY = 'jmXnMqbFRHqLtyHkms9p9jcG'
+SECRET_KEY = 'gG58DUOlh8j1bb7cff3KxVkWuX30VE6v'
 
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
+def audio_record(out_file, rec_time):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16 #16bit编码格式
+    CHANNELS = 1 #单声道
+    RATE = 16000 #16000采样频率
+    p = pyaudio.PyAudio()
+    # 创建音频流
+    stream = p.open(format=FORMAT, # 音频流wav格式
+                    channels=CHANNELS, # 单声道
+                    rate=RATE, # 采样率16000
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    print("Start Recording...")
+    frames = [] # 录制的音频流
+    # 录制音频数据
+    for i in range(0, int(RATE / CHUNK * rec_time)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    # 录制完成
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    print("Recording Done...")
+    # 保存音频文件
+    wf = wave.open(out_file, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    audio_record('C:\\Users\\zhao\\PycharmProjects\\aaa.wav',12)
+
+# 读取文件
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+#识别本地文件
+s = client.asr(get_file_content('C:\\Users\\zhao\\PycharmProjects\\aaa.wav'), 'wav', 16000, {
+        'dev_pid': 1537,})
+a = ' '.join(s['result'])
+#############################
+#文字转语音
+result  = client.synthesis('曹尼玛', 'zh', 1, {
+    'vol': 5,
+    'per': 4,
+})
+# 识别正确返回语音二进制 错误则返回dict 参照下面错误码
+if not isinstance(result, dict):
+    with open('audio.wav', 'wb') as f:
+        f.write(result)
+        print('ch')
+######################################
+#读取本地语音文件播放出来
+CHUNK = 1024
+FILENAME = 'audio.wav'
+
+def play(filename = FILENAME):
+    wf = wave.open(filename, 'rb')
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+
+    while data != b'':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+play()
+####################
